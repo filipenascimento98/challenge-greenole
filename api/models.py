96fe3e2f4d199ac827d9412cab0f8fb3baa7b0d4
Filipe <filipe.almeida@dcomp.ufs.br>
2022-12-24 16:48:49 -0300

    Criação dos models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class MeasurementData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    sensor_id = models.BigIntegerField("ID Sensor")
+    measured_at = models.DateTimeField("Measured at")
+    value = models.IntegerField("Value")
+    created_at = models.DateTimeField("Created at", auto_now_add=True)
+
+class DuplicateMeasurementData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    measurement_data = models.ForeignKey(
+        "MeasurementData",
+        verbose_name= "Duplicate measurement data",
+        related_name= "duplicate_data",
+        on_delete=models.CASCADE
+    )
